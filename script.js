8df7635baa0f14671351b37b4c47434f18d7845f
Aldrin Tarson Aloor <aldrintarson@gmail.com>
2026-03-05 00:35:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function confirmBooking(){
+
+alert("Booking confirmed. Technician will contact you soon.")
+
+window.open("https://wa.me/919000000000")
+
+}
+
+function login(){
+
+window.location="dashboard.html"
+
+}
+
+function workerSignup(){
+
+alert("Application submitted. Our team will verify your profile.")
+
+}
+
+function goAdmin(){
+
+window.location="admin.html"
+
+}
